dbb14ca45b7498bfafc00ac34abad19e02ee4c44
chathulks <sadeeshachathumal5@gmail.com>
2026-08-02 10:50:12 +0530

    add number guessing game functionality and update HTML structure

---
+++ app.js
@@ -0,0 +1,15 @@
+function gesNumber() {
+    let userNumber = document.getElementById("userNumber").value;
+    let randomNumber = Math.random() * 10 + 1;
+    let number = Math.floor(randomNumber);
+
+    if (userNumber > number) {
+        alert("Your Number Greater than in Genarate number.");
+    } else if (userNumber < number) {
+        alert("Your Number Less than in Genarate number.");
+    } else if (userNumber == number) {
+        alert("Success Full Your Number Gesing.!");
+    } else {
+        alert("Try Again..");
+    }
+}
