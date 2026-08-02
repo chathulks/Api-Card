e4441f1d92839d8bc86b1648eba7ba3ae1b90664
chathulks <sadeeshachathumal5@gmail.com>
2026-08-02 10:54:10 +0530

    fix user input validation in number guessing function

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ function gesNumber() {
     let randomNumber = Math.random() * 10 + 1;
     let number = Math.floor(randomNumber);
 
-    if (userNumber == null) {
+    console.log(number);
+    
+    if (userNumber == "") {
         alert("Please enter Your number..!");
     } else if (userNumber > number) {
         alert("Your Number Greater than in Genarate number.");
